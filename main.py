0aa860eb159e8bd65548635ca7dd86826a7f9720
hiepph <hoanghiepjp96@gmail.com>
2017-07-15 00:36:46 +0700

    refactor: Remove reloadr

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler
 from telegram.ext import Filters
 
-from reloadr import autoreload
 
 BILAC = 'https://bilac.theanht1.me/api/v2'
 
@@ -18,35 +17,30 @@ logger = logging.getLogger(__name__)
 
 
 ## Utilities
-@autoreload
 def member_info(mem):
     return "%s: %s" % (mem['username'], mem['elo'])
 
 
 ## Handlers
 def unknown(bot, update):
-    update.message.reply_text("¯\_(ツ) _/¯")
+    bot.send_message(chat_id=update.message.chat_id, text="¯\_(ツ) _/¯")
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
 
 
 ## Responses
-@autoreload
 def start(bot, update):
-    update.message.reply_text("At your service, sir.")
+    bot.send_message(chat_id=update.message.chat_id, text="At your service, sir!")
 
-@autoreload
-def ping(bot, update):
-    update.message.reply_text("Pong")
+    bot.send_message(chat_id=update.message.chat_id, text="Pong")
 
-@autoreload
 def elo(bot, update):
     r = requests.get("%s/members?sort=-elo" % BILAC)
     res= r.json()
     rep = "\n".join(map(member_info, res))
 
-    update.message.reply_text(rep)
+    bot.send_message(chat_id=update.message.chat_id, text=rep)
 
 
 if __name__ == '__main__':
